c961ebb5c1cda16893e8a37f0914803dcaeedc7b
Douglas A B Novato <douglasabnovato@gmail.com>
2021-10-08 17:23:02 -0300

    arquivos de rotas

--- src/routes.js
+++ src/routes.js
@@ -5,4 +5,8 @@ routes.get("/", (request, response) => {
     return response.sendFile(__dirname + "/views/index.html")
 })
 
+routes.get("/index.html", (req, res) => {
+    return res.redirect("/")
+})
+
 module.exports = routes;
