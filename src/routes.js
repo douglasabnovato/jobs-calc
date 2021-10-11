e16b6f9aeb8df8c20dba0e7217f193108c1eebe8
Douglas A B Novato <douglasabnovato@gmail.com>
2021-10-11 18:26:29 -0300

    template engine

--- src/routes.js
+++ src/routes.js
@@ -1,10 +1,20 @@
 const express = require("express")
-const routes = express.Router()
-const basePath = __dirname + "/views"
+const routes = express.Router() 
 
-routes.get("/", (request, response) => response.sendFile(basePath+ "/index.html"))
-routes.get("/job", (request, response) => response.sendFile(basePath+ "/job.html"))
-routes.get("/job/edit", (request, response) => response.sendFile(basePath+ "/job-edit.html"))
-routes.get("/profile", (request, response) => response.sendFile(basePath+ "/profile.html"))
+const views = __dirname + "/views/";
+
+const profile = {
+    name: "Douglas",
+    avatar: "https://avatars.githubusercontent.com/u/14242834?v=4",
+    "monthly-budge": 5000,
+    "days-per-week": 5,
+    "hours-per-day": 5,
+    "vacation-per-year": 4
+}
+
+routes.get("/", (request, response) => response.render(views + "index"))
+routes.get("/job", (request, response) => response.render(views + "job"))
+routes.get("/job/edit", (request, response) => response.render(views + "job-edit"))
+routes.get("/profile", (request, response) => response.render(views + "profile", { profile }))
  
 module.exports = routes;
