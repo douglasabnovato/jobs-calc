d6161decb4f4d1bbe8e965bd624fc3220b42617c
Douglas A B Novato <douglasabnovato@gmail.com>
2021-10-21 15:08:32 -0300

    dashboard dinamico

--- src/controllers/DashboardController.js
+++ src/controllers/DashboardController.js
@@ -5,11 +5,19 @@ const Profile = require("../model/Profile")
 module.exports = {
 
     index(request, response){
+
+        let statusCount = {
+            progress: 0,
+            done: 0,
+            total: Job.get().length,
+        }
         
         const updatedJobs = Job.get().map((job) => {
     
-            const remaining = JobUtils.remainingDays(job)
-            const status = remaining <= 0 ? "Done" : "Progress"
+            const remaining = JobUtils.remainingDays(job);
+            const status = remaining <= 0 ? "done" : "progress";
+
+            statusCount[status] += 1;
     
             return {
                 ...job,
@@ -20,7 +28,7 @@ module.exports = {
     
         }) 
     
-        return response.render("index", { jobs: updatedJobs, profile: Profile.get() }) 
+        return response.render("index", { jobs: updatedJobs, profile: Profile.get(), statusCount: statusCount }) 
     
     } 
 
