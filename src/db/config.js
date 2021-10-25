f87e3ba43e76c0bb505bf968c760713d6fa8395c
Douglas A B Novato <douglasabnovato@gmail.com>
2021-10-25 11:59:59 -0300

    configuração do db

--- src/db/config.js
+++ src/db/config.js
@@ -1,9 +1,7 @@
 const sqlite3 = require("sqlite3")
 const { open } = require("sqlite")
 
-module.exports = () => {
-    open({
-        filename: "./database.sqlite",
-        driver: sqlite3.Database,
-    });
-}
+module.exports = () => open({
+    filename: "./database.sqlite",
+    driver: sqlite3.Database, 
+}); 
--- src/model/Profile.js
+++ src/model/Profile.js
@@ -1,7 +1,7 @@
 let data = {
   name: "Douglas",
   avatar: "https://avatars.githubusercontent.com/u/14242834?v=4",
-  "monthly-budge": 6000,
+  "monthly-budget": 6000,
   "days-per-week": 5,
   "hours-per-day": 5,
   "vacation-per-year": 4,
