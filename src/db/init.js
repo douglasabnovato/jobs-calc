8df7d04d17f8c41e5c84305a9d3a1c8bc4cc6954
Douglas A B Novato <douglasabnovato@gmail.com>
2021-10-25 14:00:08 -0300

    criado db com sqlite

--- src/db/init.js
+++ src/db/init.js
@@ -6,70 +6,72 @@ const initDb = {
 
         const db = await Database(); 
 
-        await db.exec(`CREATE TABLE profile (
-            id INTEGER PRIMARY KEY AUTOINCREMENT,
-            name TEXT,
-            avatar TEXT, 
-            monthly_budget INTEGER,
-            days_per_week INTEGER,
-            hours_per_day INTEGER,
-            vacation_per_day INTEGER,
-            value_hour INTEGER
-        )`);  
-        
-        await db.exec(`CREATE TABLE jobs (
-            id INTEGER PRIMARY KEY AUTOINCREMENT,
-            name TEXT, 
-            daily_hours INT,
-            total_hours INT,
-            created_at DATETIME 
-        )`); 
-        
-        await db.run(`INSERT INTO profile(
-            name,
-            avatar, 
-            monthly_budget,
-            days_per_week,
-            hours_per_day,
-            vacation_per_day,
-            value_hour
-        ) VALUES (
-            "Jakeline",
-            "https://avatars.githubusercontent.com/u/14242834?v=4",
-            8000,
-            5,
-            5,
-            4
-        );`); 
-        
-        await db.run(`INSERT INTO jobs(
-            name, 
-            daily_hours,
-            total_hours,
-            created_at
-        ) VALUES (
-            "Pizzaria Guloso",
-            2,
-            1,
-            1617514376018 
-        );`);
+        try {
+            
+            await db.exec(`CREATE TABLE profile (
+                id INTEGER PRIMARY KEY AUTOINCREMENT,
+                name TEXT,
+                avatar TEXT, 
+                monthly_budget INT,
+                days_per_week INT,
+                hours_per_day INT,
+                vacation_per_day INT,
+                value_hour INT
+            )`);  
+            
+            await db.exec(`CREATE TABLE jobs (
+                id INTEGER PRIMARY KEY AUTOINCREMENT,
+                name TEXT, 
+                daily_hours INT,
+                total_hours INT,
+                created_at DATETIME 
+            )`); 
+            
+            await db.run(`INSERT INTO profile(
+                name,
+                avatar, 
+                monthly_budget,
+                days_per_week,
+                hours_per_day,
+                vacation_per_day,
+                value_hour ) VALUES (
+                "Jakeline",
+                "https://avatars.githubusercontent.com/u/14242834?v=4",
+                8000,
+                5,
+                5,
+                4,
+                110
+            );`); 
+            
+            await db.run(`INSERT INTO jobs(
+                name, 
+                daily_hours,
+                total_hours,
+                created_at ) VALUES (
+                "Pizzaria Guloso",
+                2,
+                1,
+                1617514376018 
+            );`);
 
-        await db.run(`INSERT INTO jobs(
-            name, 
-            daily_hours,
-            total_hours,
-            created_at
-        ) VALUES (
-            "OneTwo Project",
-            3,
-            47,
-            1617514376018
-        );`); 
-        
-        await db.close();
-             
+            await db.run(`INSERT INTO jobs(
+                name, 
+                daily_hours,
+                total_hours,
+                created_at ) VALUES (
+                "OneTwo Project",
+                3,
+                47,
+                1617514376018
+            );`); 
+            
+            await db.close();
+            
+        }catch(error){ console.log(error.message) }  
+      
     } 
  
 }
 
-initDb.init()
+initDb.init(); 
