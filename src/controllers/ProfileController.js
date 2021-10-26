9e67d33ca142a62bef70da52505c3b81f51c8b33
Douglas A B Novato <douglasabnovato@gmail.com>
2021-10-26 11:12:01 -0300

    corrigindo detalhes

--- src/controllers/ProfileController.js
+++ src/controllers/ProfileController.js
@@ -2,38 +2,31 @@ const Profile = require("../model/Profile")
 
 module.exports = {
     
-    async index(request, response){
-        
-        try{ 
-            
-            return response.render("profile", { profile: await Profile.get() })
+    async index(request, response){ 
 
-        }catch(error){ console.log(error.message) }
-
+        return response.render("profile", { profile: await Profile.get() })
+ 
     },
 
-    async update(request, response){
+    async update(request, response){ 
 
-        try{
+        const data = request.body;
+        const weeksPerYear = 52;
+        const weeksPerMonth = (weeksPerYear - data["vacation-per-year"]) / 12;
+        const weekTotalHours = data["hours-per-day"] * data["hours-per-week"];
+        const monthlyTotalHours = weekTotalHours * weeksPerMonth;
 
-            const data = request.body;
-            const weeksPerYear = 52;
-            const weeksPerMonth = (weeksPerYear - data["vacation-per-year"]) / 12;
-            const weekTotalHours = data["hours-per-day"] * data["hours-per-week"];
-            const monthlyTotalHours = weekTotalHours * weeksPerMonth;
+        const valueHour = data["monthly-budget"] / monthlyTotalHours
 
-            const valueHour = data["monthly-budget"] / monthlyTotalHours
+        const profile = await Profile.get();
 
-            const profile = await Profile.get();
+        await Profile.update({
+            ...profile,
+            ...request.body,
+            "value-hour": valueHour
+        }) 
 
-            await Profile.update({
-                ...profile,
-                ...request.body,
-                "value-hour": valueHour
-            }) 
-
-            return response.redirect("/profile")
-            
-        }catch(error){ console.log(error.message) }
+        return response.redirect("/profile")
+             
     },
 } 
--- src/model/Profile.js
+++ src/model/Profile.js
@@ -5,46 +5,38 @@ module.exports = {
     async get(){
  
         const db = await Database(); 
-
-        try{ 
  
-            const data = await db.get(`SELECT * FROM profile`);
+        const data = await db.get(`SELECT * FROM profile`);
+        
+        await db.close(); 
     
-            await db.close(); 
-     
-            return {
-                name: data.name,
-                avatar: data.avatar,
-                "monthly-budget": data.monthly_budget,
-                "days-per-week": data.days_per_week,
-                "hours-per-day": data.hours_per_day,
-                "vacation-per-year": data.vacation_per_year,
-                "value-hour": data.value_hour
-            };
-
-        }catch(error){ console.log(error.message) }
+        return {
+            name: data.name,
+            avatar: data.avatar,
+            "monthly-budget": data.monthly_budget,
+            "days-per-week": data.days_per_week,
+            "hours-per-day": data.hours_per_day,
+            "vacation-per-year": data.vacation_per_year,
+            "value-hour": data.value_hour
+        }; 
  
     },
 
     async update(newData){
 
         const db = await Database();
+  
+        db.run(`UPDATE profile SET
+        name = "${newData.name}",
+        avatar = "${newData.avatar}",
+        monthly_budget = ${newData["monthly-budget"]},
+        days_per_week = ${newData["days-per-week"]},
+        hours_per_day = ${newData["hours-per-day"]},
+        vacation_per_year = ${newData["vacation-per-year"]},
+        value_hour = ${newData["value-hour"]}
+        `)
 
-        try{
- 
-            db.run(`UPDATE profile SET
-            name = "${newData.name}",
-            avatar = "${newData.avatar}",
-            monthly_budget = ${newData["monthly-budget"]},
-            days_per_week = ${newData["days-per-week"]},
-            hours_per_day = ${newData["hours-per-day"]},
-            vacation_per_year = ${newData["vacation-per-year"]},
-            value_hour = ${newData["value-hour"]}
-            `)
-
-            await db.close(); 
-            
-        }catch(error){ console.log(error.message) }
-
+        await db.close(); 
+              
     }
 } 
